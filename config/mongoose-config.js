87b7bff5245b5df2da5c72de84851cc0bddef82f
Arpit Paliwal <133906733+Arpit0408@users.noreply.github.com>
2025-07-15 07:47:09 +0530

    Update mongoose-config.js

--- config/mongoose-config.js
+++ config/mongoose-config.js
@@ -3,7 +3,7 @@ const config = require("config");
 const dbgr = require("debug")("development:mongoose")
 mongoose.connect(`${config.get("MONGODB_URI")}`)
 .then(function () {
-console.log("✅ MongoDB connected"))    
+console.log("✅ MongoDB connected")
 })
 .catch(function(err){
  console.error("❌ MongoDB connection error:", err); // ✅ Proper logging})
