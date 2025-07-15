a97576744a311fbe14fe5a582abd51f11f3a8c14
Arpit Paliwal <133906733+Arpit0408@users.noreply.github.com>
2025-07-15 10:12:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -71,5 +71,5 @@ app.get('/health', (req, res) => {
 
 
 // ✅ Start server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
