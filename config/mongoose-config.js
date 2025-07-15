4512fff9390eca153da88775f69faa2a79acb27d
Arpit Paliwal <133906733+Arpit0408@users.noreply.github.com>
2025-07-15 07:09:12 +0530

    Update mongoose-config.js

--- config/mongoose-config.js
+++ config/mongoose-config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const config = require("config");
 const dbgr = require("debug")("development:mongoose")
-mongoose.connect(`${config.get("MONGODB_URI")}`)
+mongoose.connect(`${config.get("mongodb+srv://paliwalarpit180:oyANJmADfmOAKiUy@cluster0.ybr1tgz.mongodb.net/buykrao?retryWrites=true&w=majority")}`)
 .then(function () {
     dbgr("connected");
     
